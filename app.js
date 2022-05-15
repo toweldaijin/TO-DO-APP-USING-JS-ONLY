7de9de50a84d2c24c4b62f362ce8ce098ff7fada
Sayaka K <colourfulflower21@gmail.com>
2022-05-16 01:50:48 +0900

    34,35

--- app.js
+++ app.js
@@ -11,32 +11,45 @@ const onClickAdd = () => {
     /* div生成 */
     const div = document.createElement("div");
     div.className = "listRow";
-    div.innerText = inputText;
+    const p = document.createElement("p");
+    p.className = "taskTitle";
+    p.textContent = inputText;
 
     /* Button(完了)作成 */
     const completeButton = document.createElement("button");
     completeButton.innerText = "完了";
     completeButton.addEventListener("click", ()=>{
-        alert("完了");
-    });
+        /* 押された完了ボタンの親タグ(li)を未完了リストから削除 */
+        deleteFromImcompleteList(completeButton.parentNode);
+        /* 完了リストに追加する要素 */
+        const parentOfAddTarget = completeButton.parentNode;
+        const addTarget = parentOfAddTarget.parentNode;
+        const text = parentOfAddTarget.firstElementChild.innerText;
+        console.log(text);
+    });   
 
     /* Button(削除)作成 */
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "削除";
     deleteButton.addEventListener("click", ()=>{
         /* 押された削除ボタンの親タグ(li)を未完了リストから削除 */
-        const parentsOfDiv = deleteButton.parentNode; 
-        const deleteTarget = parentsOfDiv.parentNode;
-        document.getElementById("imcompleteList").removeChild(deleteTarget);
+        deleteFromImcompleteList(deleteButton.parentNode);
     });
 
    /* liタグの子要素に各要素を設定 */ 
    li.appendChild(div);
-   div.appendChild(completeButton);
-   div.appendChild(deleteButton);
+    div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
     
    /* 未完了リストに追加 */
    document.getElementById("imcompleteList").appendChild(li);
+};
+
+/* 未完了リストから指定の要素を削除 */
+function deleteFromImcompleteList(target) {
+    const deleteTarget = target.parentNode;
+    document.getElementById("imcompleteList").removeChild(deleteTarget);
 }
 
 document
